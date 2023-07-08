0b37e058c1ec9f62ff61d8386aefdba4598ff118
Raja Kumar <rajakumarmahto952@gmail.com>
2023-07-08 22:32:13 +0530

    added firebase

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSy76QJEq3kCD_dueWNHyVqttbWzUUeQk",
+  authDomain: "real-estate-27033.firebaseapp.com",
+  projectId: "real-estate-27033",
+  storageBucket: "real-estate-27033.appspot.com",
+  messagingSenderId: "287502784251",
+  appId: "1:287502784251:web:7f10ea40e3d1168b9e425e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
