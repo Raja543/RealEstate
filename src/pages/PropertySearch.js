863128afbea72ffa2f952e918ab4fededfa78fae
Raja Kumar <rajakumarmahto952@gmail.com>
2023-08-22 23:42:37 +0530

    designing of house cards updated

--- src/pages/PropertySearch.js
+++ src/pages/PropertySearch.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { getDatabase, ref, get, push, set } from "firebase/database";
+import { getDatabase, ref, get } from "firebase/database";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import CarouselBanner from "../components/CarouselBanner/CarouselBanner";
-import { NavLink } from "react-router-dom";
+import { AreaChart, Building2, MapPin } from "lucide-react";
+import { IndianRupee } from "lucide-react";
 
 const PropertySearch = () => {
+  const [housename] = useState("");
   const [location, setLocation] = useState("");
-  const [priceRange, setPriceRange] = useState("");
+  const [price, setPrice] = useState("");
   const [propertyType, setPropertyType] = useState("");
+  const [furnitureType, setfurnitureType] = useState("");
   const [bedrooms, setBedrooms] = useState("");
-  const [properties, setProperties] = useState([]);
+  const [area, setArea] = useState("");
+  const [displayProperties, setDisplayProperties] = useState([]);
   const [matchingProperties, setMatchingProperties] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -19,10 +23,13 @@ const PropertySearch = () => {
     e.preventDefault();
 
     const searchCriteria = {
+      housename,
       location,
-      priceRange,
+      price,
       propertyType,
       bedrooms,
+      area,
+      furnitureType,
     };
 
     try {
@@ -62,7 +69,7 @@ const PropertySearch = () => {
 
         if (
           searchCriteria.priceRange &&
-          property.priceRange !== searchCriteria.priceRange
+          property.price !== searchCriteria.price
         ) {
           return false;
         }
@@ -81,7 +88,9 @@ const PropertySearch = () => {
           return false;
         }
 
-        // Add other filtering conditions as needed
+        if (searchCriteria.area && property.area !== searchCriteria.area) {
+          return false;
+        }
 
         return true;
       });
@@ -95,34 +104,11 @@ const PropertySearch = () => {
     }
   };
 
-  const storePropertyData = async () => {
-    try {
-      const db = getDatabase();
-      const propertiesRef = ref(db, "properties");
-      const newPropertyRef = push(propertiesRef);
-      await set(newPropertyRef, {
-        location,
-        priceRange,
-        propertyType,
-        bedrooms,
-      });
-
-      setLocation("");
-      setPriceRange("");
-      setPropertyType("");
-      setBedrooms("");
-
-      console.log("Property data stored successfully!");
-    } catch (error) {
-      console.log("Error storing property data:", error);
-    }
-  };
-
   useEffect(() => {
     const fetchProperties = async () => {
       try {
-        setLoading(true); // Set loading to true while fetching data
-        setError(null); // Clear any previous error
+        setLoading(true);
+        setError(null);
         const db = getDatabase();
         const propertiesRef = ref(db, "properties");
         const snapshot = await get(propertiesRef);
@@ -133,15 +119,27 @@ const PropertySearch = () => {
             ...childSnapshot.val(),
           });
         });
-        setProperties(allProperties);
+
+        // Randomly select 6 properties
+        const shuffledProperties = shuffleArray(allProperties);
+        const selectedProperties = shuffledProperties.slice(0, 6);
+        setDisplayProperties(selectedProperties);
       } catch (error) {
         setError("Error fetching properties");
       } finally {
-        setLoading(false); // Set loading back to false after fetching data
+        setLoading(false);
       }
     };
     fetchProperties();
   }, []);
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
   return (
     <>
@@ -149,168 +147,219 @@ const PropertySearch = () => {
       <div height={200} width={300}>
         <CarouselBanner />
       </div>
-      <div className="container flex-col mx-auto px-4 py-8">
-        <div className="flex-row">
-          <ul
-            className={`md:flex md:items-center hidden space-x-3  "block" : "hidden"
-            }`}
-          >
-            <NavLink to="/">
-              <li className="text-gray-800 text-xl hover:text-gray-500 pr-4">
-                Home
-              </li>
-            </NavLink>
-            <NavLink to="/findproperty">
-              <li className="text-gray-800 text-xl hover:text-gray-500 pr-4">
-                Find Property
-              </li>
-            </NavLink>
-            <NavLink to="/listproperty">
-              <li className="text-gray-800 text-xl hover:text-gray-500 pr-2">
-                List Property
-              </li>
-            </NavLink>
-            <NavLink to="/agents">
-              <li className="text-gray-800 text-xl hover:text-gray-500 pr-2">
-                Agents
-              </li>
-            </NavLink>
-            <NavLink to="/contacts">
-              <li className="text-gray-800 text-xl hover:text-gray-500 pr-2">
-                Contact Us
-              </li>
-            </NavLink>
-          </ul>
-        </div>
-        <div className="flex justify-between items-center">
+
+      {/* search bar */}
+      <div
+        className="container  flex-col z-20 mx-auto w-3/5 px-4 py-8 mt-[-30px] bg-textwhite rounded-lg shadow-xl"
+        style={{ position: "relative" }}
+      >
+        <div className="flex justify-between items-center ">
+          <div className="mb-4">
+            <label htmlFor="propertyType" className="block mb-1">
+              Property Type:
+            </label>
+            <select
+              id="propertyType"
+              value={propertyType}
+              onChange={(e) => setPropertyType(e.target.value)}
+              className="border border-gray-300 px-2 py-1 rounded"
+            >
+              <option value="">Any</option>
+              <option value="residential">Residential</option>
+              <option value="pg/co-liing">PG/Co-living</option>
+              <option value="commercial">Commercial</option>
+            </select>
+          </div>
           <input
-            type="search"
-            placeholder="Search"
-            className="border border-gray-300 px-2 py-1 rounded"
+            type="text"
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className="border border-gray-300 px-2 py-1 rounded w-2/4"
           />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleSearch}
+          >
             Search
           </button>
         </div>
+        <div className="flex flex-row justify-between items-center ">
+          <div className="mb-4">
+            <label htmlFor="price" className="block mb-1">
+              Price :
+            </label>
+            <input
+              type="number"
+              id="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              className="border border-gray-300 px-2 py-1 rounded"
+            ></input>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="propertyType" className="block mb-1">
+              Furniture type:
+            </label>
+            <select
+              id="furnitureType"
+              value={furnitureType}
+              onChange={(e) => setfurnitureType(e.target.value)}
+              className="border border-gray-300 px-2 py-1 rounded"
+            >
+              <option value="">Any</option>
+              <option value="Non-furnished">Non-furnished</option>
+              <option value="Semi-furnished">Semi-furnished</option>
+              <option value="Full-furnished">Full-furnished</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="bedrooms" className="block mb-1">
+              Bedrooms:
+            </label>
+            <select
+              id="bedrooms"
+              value={bedrooms}
+              onChange={(e) => setBedrooms(e.target.value)}
+              className="border border-gray-300 px-2 py-1 rounded"
+            >
+              <option value="">Any</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="area" className="block mb-1">
+              Area:
+            </label>
+            <input
+              type="number"
+              id="area"
+              value={area}
+              onChange={(e) => setArea(e.target.value)}
+              className="border border-gray-300 px-2 py-1 rounded"
+            ></input>
+          </div>
+        </div>
       </div>
 
-      <div className="container mx-auto px-4 py-8">
-        <h2 className="text-2xl font-semibold mb-4">Property Search</h2>
-        <div className="mb-4">
-          <form onSubmit={handleSearch}>
-            <div className="mb-4">
-              <label htmlFor="location" className="block mb-1">
-                Location:
-              </label>
-              <input
-                type="text"
-                id="location"
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-                className="border border-gray-300 px-2 py-1 rounded"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="priceRange" className="block mb-1">
-                Price Range:
-              </label>
-              <select
-                id="priceRange"
-                value={priceRange}
-                onChange={(e) => setPriceRange(e.target.value)}
-                className="border border-gray-300 px-2 py-1 rounded"
-              >
-                <option value="">Any</option>
-                <option value="0-100000">$0 - $100,000</option>
-                <option value="100000-200000">$100,000 - $200,000</option>
-                <option value="200000-300000">$200,000 - $300,000</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label htmlFor="propertyType" className="block mb-1">
-                Property Type:
-              </label>
-              <select
-                id="propertyType"
-                value={propertyType}
-                onChange={(e) => setPropertyType(e.target.value)}
-                className="border border-gray-300 px-2 py-1 rounded"
-              >
-                <option value="">Any</option>
-                <option value="house">House</option>
-                <option value="apartment">Apartment</option>
-                <option value="condo">Condo</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label htmlFor="bedrooms" className="block mb-1">
-                Bedrooms:
-              </label>
-              <select
-                id="bedrooms"
-                value={bedrooms}
-                onChange={(e) => setBedrooms(e.target.value)}
-                className="border border-gray-300 px-2 py-1 rounded"
-              >
-                <option value="">Any</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </div>
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            >
-              Search
-            </button>
-            <button
-              type="button"
-              onClick={storePropertyData}
-              className="bg-green-500 text-white px-4 py-2 rounded ml-4 hover:bg-green-600"
-            >
-              Save Property
-            </button>
-          </form>
+      {/* display type of property */}
+      <div className="px-10  my-4 ">
+        <h1 className="text-2xl text-center p-2">
+          Get Started with real estate options
+        </h1>
+        <div className="flex flex-row items-center justify-center">
+          <div className="flex flex-col mx-6 my-4">
+            <img src="/images/d1.webp" alt="display1"></img>
+            <p className="text-sm text-center p-4">Buying a home</p>
+          </div>
+          <div className="flex flex-col  mx-6 my-4 ">
+            <img src="/images/d2.webp" alt="display1"></img>
+            <p className="text-sm text-center p-4">Renting a home</p>
+          </div>
+          <div className="flex flex-col  mx-6 my-4">
+            <img src="/images/d3.webp" alt="display1"></img>
+            <p className="text-sm text-center p-4">Sell/Rent your Property</p>
+          </div>
+          <div className="flex flex-col  mx-6 my-4">
+            <img src="/images/d4.webp" alt="display1"></img>
+            <p className="text-sm text-center p-4">PG and co-living</p>
+          </div>
+          <div className="flex flex-col  mx-6 my-4">
+            <img src="/images/d5.webp" alt="display1"></img>
+            <p className="text-sm text-center p-4">Buying commercial space</p>
+          </div>
         </div>
+      </div>
 
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl text-center p-4 font-extrabold max-w-xl">
+          Find Better Places to Live, Work and Wonder...
+        </h1>
+      </div>
+
+      {/* display house card */}
+      <div>
         <div className="mt-8">
-          {/* Render loading message while fetching data */}
           {loading && <p>Loading...</p>}
-
-          {/* Render error message if there was an error */}
           {error && <p>{error}</p>}
-
-          {/* Conditionally render properties based on whether a search is performed */}
-          {matchingProperties.length === 0
-            ? properties.map((property) => (
-                <div
-                  key={property.id}
-                  className="border border-gray-200 rounded p-4 mb-4"
-                >
-                  <h2 className="text-xl font-bold mb-2">
-                    {property.location}
-                  </h2>
-                  <p className="text-gray-600 mb-2">
-                    Price: {property.priceRange}
-                  </p>
-                  <p className="text-gray-600 mb-2">
-                    Type: {property.propertyType}
-                  </p>
-                  <p className="text-gray-600 mb-2">
-                    Bedrooms: {property.bedrooms}
-                  </p>
-                </div>
-              ))
-            : matchingProperties.map((property) => (
-                <div key={property.id} className="bg-gray-100 p-4 mb-4">
-                  <p className="mb-2">Price: {property.priceRange}</p>
-                  <p className="mb-2">Location: {property.location}</p>
-                  <p className="mb-2">Type: {property.propertyType}</p>
-                  <p className="mb-2">Bedrooms: {property.bedrooms}</p>
-                  {/* ... Render other property details ... */}
-                </div>
-              ))}
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-16 mx-32">
+            {matchingProperties.length === 0
+              ? displayProperties.map((property) => (
+                  <div
+                    key={property.id}
+                    className="bg-white shadow-xl  rounded-lg overflow-hidden cursor-pointer "
+                  >
+                    <div
+                      className="bg-cover bg-center h-56 p-4"
+                      style={{
+                        backgroundImage:
+                          "url(https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80)",
+                      }}
+                    ></div>
+                    <div className="p-4">
+                      <p className="Capitalize text-left text-2xl font-extrabold py-2">
+                        {property.housename}
+                      </p>
+                      <div className="flex flex-row items-center justify-between">
+                        <div className="text-lg flex flex-row ">
+                          <IndianRupee /> {property.price}
+                        </div>
+                        <div className=" text-lg flex flex-row ">
+                          <AreaChart /> {property.area} sqft
+                        </div>
+                      </div>
+                      <div className="flex flex-row items-center justify-between my-2">
+                        <div className="text-lg flex flex-row">
+                          <MapPin /> {property.location}
+                        </div>
+                        <div className="text-lg flex flex-row">
+                          <Building2 /> {property.propertyType}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : matchingProperties.map((property) => (
+                  <div
+                    key={property.id}
+                    className="bg-white shadow-xl  rounded-lg overflow-hidden"
+                  >
+                    <div
+                      className="bg-cover bg-center h-56 p-4"
+                      style={{
+                        backgroundImage:
+                          "url(https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80)",
+                      }}
+                    >
+                      <div className="flex justify-end">
+                        <svg
+                          className="h-6 w-6 text-white fill-current"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z"></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="p-4">
+                      <p className="uppercase tracking-wide text-sm font-bold text-gray-700">
+                        <Building2 /> {property.housename}
+                      </p>
+                      <div className="text-3xl text-gray-900 flex-row">
+                        <IndianRupee /> {property.price}
+                      </div>
+                      <div className="text-gray-700 flex-row">
+                        <MapPin /> {property.location}
+                      </div>
+                      <div>
+                        <p className="text-gray">{property.area}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+          </div>
         </div>
       </div>
       <Footer />
