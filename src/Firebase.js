a1926985b8d3c8b6f4b222766cc8ccef179e84cd
Raja Kumar <rajakumarmahto952@gmail.com>
2023-12-09 20:54:50 +0530

    removed firebase config info

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth , GoogleAuthProvider } from "firebase/auth";
-import { getDatabase } from 'firebase/database';
-
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUnqAudEZCiiEeKr0wLlFhOOwD1RaqtCI",
-  authDomain: "real-estate-f5d9c.firebaseapp.com",
-  projectId: "real-estate-f5d9c",
-  storageBucket: "real-estate-f5d9c.appspot.com",
-  databaseURL: 'https://real-estate-f5d9c-default-rtdb.asia-southeast1.firebasedatabase.app',
-  messagingSenderId: "784322307430",
-  appId: "1:784322307430:web:7a90f6b443c199ec0254a7",
+  //Your Firebase config here
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  databaseURL: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
@@ -25,6 +25,4 @@ const provider = new GoogleAuthProvider();
 
 const database = getDatabase(app);
 
-export { db , auth , provider , database };
-
-
+export { db, auth, provider, database };
